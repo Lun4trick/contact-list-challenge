b2f4e24cdb268a1831f5ada4e5c530722ac840ef
Lun4trick <krotos.levi@gmail.com>
2024-08-26 14:16:15 +0300

    added coming soon toast to buttons, what dont have functionality

--- app/hooks/useContactContainerButtons.ts
+++ app/hooks/useContactContainerButtons.ts
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { ContactContainerButtonType } from '../types/contactContainerButtonType'
+import toast from 'react-hot-toast'
 
 function useContactContainerButtons() {
   const [isOptionsOpen, setIsOptionsOpen] = useState(false)
@@ -17,14 +18,14 @@ function useContactContainerButtons() {
         name: 'notifications',
         icon: '/svgs/notification-icon.svg',
         type: 'NOTIFICATIONS' as ContactContainerButtonType,
-        onClick: () => console.log('notifications')
+        onClick: () => toast.loading('Coming soon!', {style: {background: '#333333', color: '#ffffff'}, duration: 1500})
       },
 
       {
         name: 'headphone',
         icon: '/svgs/headphone-icon.svg',
         type: 'HEADPHONE' as ContactContainerButtonType,
-        onClick: () => console.log('headphone')
+        onClick: () => toast.loading('Coming soon!', {style: {background: '#333333', color: '#ffffff'}, duration: 1500})
       },
       {
         name: 'options',
