39856d6714b1f369b71e39d22b30437386adce9b
Lun4trick <krotos.levi@gmail.com>
2024-08-26 23:07:46 +0300

    still try to disable caching

--- app/api/imgs/route.ts
+++ app/api/imgs/route.ts
@@ -2,8 +2,6 @@ import { GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import s3 from "@/app/utils/s3Bucket";
 
-export const fetchCache = 'force-no-store';
-
 export async function GET() {
   try {
     const command = new GetObjectCommand({
@@ -11,12 +9,16 @@ export async function GET() {
       Key: 'Default.png',
     });
 
-    const url = await getSignedUrl(s3, command, { expiresIn: 3 });
+    const url = (await getSignedUrl(s3, command, { expiresIn: 3}));
     console.log('url:', url);
 
     const response = new Response(url, {
       headers: {
         'Content-Type': 'text/plain',
+        'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+        'Pragma': 'no-cache',
+        'Expires': '0',
+        'Surrogate-Control': 'no-store'
       }
     });
 
